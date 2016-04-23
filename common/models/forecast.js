7858cc8cd9eac6af9828499f8add1bdc7f647563
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2016-04-23 13:07:43 +0300

    added forecast api and public server

--- common/models/forecast.js
+++ common/models/forecast.js
@@ -1,13 +1,18 @@
+var NodeCache = require( "node-cache" );
+
 module.exports = function(Forecast) {
 
-  Forecast.fetchForecast = function(cb) {
-    cb(null, {})
-  };
+
 
   Forecast.remoteMethod(
-    'fetchForecast',
+    'find',
     {
       description: 'fetch forecast',
+      accepts: [
+        {arg: 'lat', type: 'number', required: true},
+        {arg: 'lon', type: 'number', required: true},
+        {arg: 'token', type: 'string', required: true}
+      ],
       http: {path: '/',verb: 'get'},
       returns: {type: 'Forecast', root: true}
     }
