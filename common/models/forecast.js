281937e2f904282a5177a0816f016d05a5f272af
Ville-Matti Riihikoski <villematti.riihikoski@gmail.com>
2016-12-05 20:47:08 +0200

    forecast done

--- common/models/forecast.js
+++ common/models/forecast.js
@@ -1,17 +1,54 @@
 const NodeCache = require( "node-cache" );
+const parseString = require('xml2js').parseString;
 
 module.exports = function(Forecast) {
 
+  Forecast.getForecast = function(next) {
+    Forecast.find(function(err, data) {
+      if (err) return next(err);
+      parseString(data, function(err, result) {
+        if (err) return next(err);
+        let results = {};
+        if (result && result['wfs:FeatureCollection'] && result['wfs:FeatureCollection']['wfs:member']) {
+          result['wfs:FeatureCollection']['wfs:member'].map(item => {
+            item['BsWfs:BsWfsElement'].map(elem => {
+              if (!elem['BsWfs:Time'] || !elem['BsWfs:Time'][0] ||
+                !elem['BsWfs:ParameterName'] || !elem['BsWfs:ParameterName'][0] ||
+                !elem['BsWfs:ParameterValue'] || !elem['BsWfs:ParameterValue'][0]) {
+                return;
+              }
+              if (!results[elem['BsWfs:Time'][0]]) {
+                results[elem['BsWfs:Time'][0]] = {};
+              }
+              results[elem['BsWfs:Time'][0]][elem['BsWfs:ParameterName'][0]] = elem['BsWfs:ParameterValue'][0]
+            })
+          });
+        }
+        let list = [];
+        for(let i in results) {
+          if (!results.hasOwnProperty(i)) {
+            continue;
+          }
+          let obj = results[i];
+          obj['time'] = i;
+          list.push(obj)
+        }
+        next(null, list);
+      });
+
+    });
+  };
+
   Forecast.remoteMethod(
-    'find',
+    'getForecast',
     {
       description: 'fetch forecast',
-      accepts: [
-        {arg: 'lat', type: 'number', required: true},
-        {arg: 'lon', type: 'number', required: true}
-      ],
       http: {path: '/',verb: 'get'},
       returns: {type: 'Forecast', root: true}
     }
   );
+
+  Forecast.disableRemoteMethodByName("find", true);
+  Forecast.disableRemoteMethodByName("invoke", true);
+
 };
